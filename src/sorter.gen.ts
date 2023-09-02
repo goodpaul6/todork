6a1541fdb1caa93cd046182e2d13d7b0f93ba241
Apaar Madan <apaar_madan@hotmail.com>
2023-09-01 23:42:48 -0400

    Remove generated code

--- src/sorter.gen.ts
+++
@@ -1,9 +0,0 @@
-/* TypeScript file generated from sorter.res by genType. */
-/* eslint-disable import/first */
-
-
-// @ts-ignore: Implicit any on import
-import * as sorterBS__Es6Import from './sorter.bs';
-const sorterBS: any = sorterBS__Es6Import;
-
-export const sortLines: (lines:string[]) => string[] = sorterBS.sortLines;
